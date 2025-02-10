817f091c96e206b024869e994aaac980fce3960d
erwinsetyaaa <erwinsetya34@gmail.com>
2025-02-11 06:34:25 +0700

    Update 11 Feb

--- server.js
+++ server.js
@@ -11,12 +11,32 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-// CONTACT ME
-app.get("/", (req, res) => {
-  res.render("contact");
-});
+// // CONTACT ME
+// app.get("/", (req, res) => {
+//   res.render("contact");
+// });
 
 // BLOG
 app.get("/", (req, res) => {
   res.render("blog");
+});
+
+
+
+
+
+// REQUEST PARAMS
+app.get("/about/:id", (req, res) => {
+  const id = req.params.id;
+  res.send(`Halo, ini halaman tentang ${id}`);
+});
+
+// REQUEST QUERY
+app.get("/blog", (req, res) => {
+  const { title, author, year } = req.query;
+  res.send(`ini halaman blog ${title} dengan author ${author} tahun ${year}`);
+});
+
+app.listen(port, () => {
+  console.log(`My personal web app listening on port ${port}`);
 });
